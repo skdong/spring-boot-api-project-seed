86c63305f4ee000752be30f9f75d2a41d3eaa51c
Administrator <test@test.com>
2019-06-09 17:43:20 +0800

    throw interupt exception

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -3,10 +3,13 @@ package com.company.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.company.project.configurer.ServerListener;
+
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
     }
+    
 }
 
