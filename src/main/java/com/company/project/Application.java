c4cff09249053c8a83b94e93c663b3d8baf8312e
Administrator <test@test.com>
2019-06-24 17:00:44 +0800

    fix autoconfig bug for idea catch socket reset exception

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -3,8 +3,6 @@ package com.company.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.company.project.configurer.ServerListener;
-
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws InterruptedException {
